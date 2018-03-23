3505412f31491b54f348a0c3aec9f98bc984910b
Jeremy Fortune <jeremy@careevolution.com>
2018-03-23 10:24:26 -0400

    Various GitSparklyClean Features
    - Reorder emoji because you wouldn't flip a table before yuno
    - Support multiple excludes
    - Add default .idea exclude
    - Support dryrun

--- Functions/Invoke-GitSparklyClean.ps1
+++ Functions/Invoke-GitSparklyClean.ps1
@@ -1,20 +1,39 @@
 function Invoke-GitSparklyClean {
-	param(
-		[String] $Exclude
+	[CmdletBinding()] param(
+		[String[]] $Excludes,
+		[Switch] $DryRun
 	)
 	$count = 0
-	$results = 1
-	while ( $results -gt 0 ) {
-		if ( $count -eq 2 ){
-			Write-Host '(╯°□°)╯︵ ┻━┻'
-		} elseif ( $count -eq 3 ) {
+	$results = @( "default" )
+
+	if ( $DryRun ) {
+		$dryRunFlag = "-n"
+	}
+	if ( $Excludes.Count -gt 0 ) {
+		foreach ( $exclude in $Excludes ) {
+			$excludeCommand = "$excludeCommand -e $exclude"
+		}
+	}
+	$defaultExclude = "-e .idea"
+
+	$command = "git clean -xdf $defaultExclude $excludeCommand $dryRunFlag"
+	Write-Verbose "Executing '$command'"
+
+	while ( $results.Count -gt 0 ) {
+		if ( $count -eq 2 ) {
 			Write-Host 'ლ(ಠ益ಠლ)'
+		} elseif ( $count -eq 3 ) {
+			Write-Host '(╯°□°)╯︵ ┻━┻'
 		}
-		if ( $Exclude ) {
-			$results = ( git clean -xdfe $Exclude ).Count
-		} else {
-			$results = ( git clean -xdf ).Count
+
+		$results = Invoke-Expression $command
+		if ( $results ) {
+			Write-Verbose $results
 		}
+		if ( $DryRun ) {
+			$results = @()
+		}
+
 		$count++
 	}
 }
