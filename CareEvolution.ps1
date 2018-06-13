4c72638d5223653a49edf43b6f9c5920228631e3
Jeremy Fortune <jeremy@careevolution.com>
2018-06-13 09:34:26 -0400

    Add B3 Galileo Environment Vars

--- CareEvolution.ps1
+++ CareEvolution.ps1
@@ -19,6 +19,11 @@ $Global:CredentialStore.Ce = Get-StoredCredential -Type Generic -WarningAction S
 $Global:CredentialStore.CeCorp = Get-StoredCredential -Type Generic -WarningAction SilentlyContinue -ErrorAction SilentlyContinue -Target "adfs.careevolution.com"
 $Global:CredentialStore.Trinity = Get-StoredCredential -Type Generic -WarningAction SilentlyContinue -ErrorAction SilentlyContinue -Target "devidp.trinity-health.org"
 
+$Env:B3POSH_URL = "https://b3.careevolution.com"
+$Env:B3POSH_API_KEY = ( Get-StoredCredential -Type Generic -WarningAction SilentlyContinue -ErrorAction SilentlyContinue -Target "b3.careevolution.com" ).GetNetworkCredential().Password
+$Env:OCTOPUS_SERVERURL = "https://octopus.careevolution.com"
+$Env:OCTOPUS_APIKEY = $CredentialStore.Tokens.OctopusApiKey
+
 if ( Test-Path $Home\CeServers.json ){
   $Global:CeServers = Get-Content $Home\CeServers.json | ConvertFrom-Json
 }
