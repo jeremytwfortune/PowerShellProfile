5d9f0e22892e4277a3a5651cf886dacbe619d3e1
Jeremy Fortune <jeremy@careevolution.com>
2019-09-17 14:45:41 -0400

    Add Clear-AwsDefaultSession
    Default sessions were not properly storing in the machine registry.
    Additionally, add a clear function so that requesting another key is
    simpler.

--- CEFunctions/Set-AwsDefaultSession.ps1
+++ CEFunctions/Set-AwsDefaultSession.ps1
@@ -5,26 +5,20 @@ function Set-AwsDefaultSession {
 		[string] $TokenCode,
 
 		[Parameter()]
+		[System.EnvironmentVariableTarget] $Environment = [System.EnvironmentVariableTarget]::Process,
+
+		[Parameter()]
 		[string] $SerialNumber = "arn:aws:iam::174627156110:mfa/jeremy",
 
 		[Parameter()]
-		[switch] $PassThru,
-
-		[Parameter()]
-		[switch] $System
+		[switch] $PassThru
 	)
 
 	$token = Get-STSSessionToken -SerialNumber $SerialNumber -TokenCode $TokenCode
 
-	if ($System) {
-		[Environment]::SetEnvironmentVariable("AWS_ACCESS_KEY_ID", $token.AccessKeyId, [System.EnvironmentVariableTarget]::User)
-		[Environment]::SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", $token.SecretAccessKey, [System.EnvironmentVariableTarget]::User)
-		[Environment]::SetEnvironmentVariable("AWS_SESSION_TOKEN", $token.SessionToken, [System.EnvironmentVariableTarget]::User)
-	} else {
-		$Env:AWS_ACCESS_KEY_ID = $token.AccessKeyId
-		$Env:AWS_SECRET_ACCESS_KEY = $token.SecretAccessKey
-		$Env:AWS_SESSION_TOKEN = $token.SessionToken
-	}
+	[Environment]::SetEnvironmentVariable("AWS_ACCESS_KEY_ID", $token.AccessKeyId, $Environment)
+	[Environment]::SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", $token.SecretAccessKey, $Environment)
+	[Environment]::SetEnvironmentVariable("AWS_SESSION_TOKEN", $token.SessionToken, $Environment)
 
 	if ($PassThru) {
 		$token
