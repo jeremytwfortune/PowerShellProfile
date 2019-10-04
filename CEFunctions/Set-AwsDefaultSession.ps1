8ba57c6046b5893601cfd74650395449a789134a
Jeremy Fortune <jeremy@careevolution.com>
2019-10-04 15:40:13 -0400

    Add TokenCode Prompt

--- CEFunctions/Set-AwsDefaultSession.ps1
+++ CEFunctions/Set-AwsDefaultSession.ps1
@@ -1,8 +1,8 @@
 function Set-AwsDefaultSession {
 	[CmdletBinding()]
 	param(
-		[Parameter(Mandatory)]
-		[string] $TokenCode,
+		[ValidateNotNullOrEmpty()]
+		[string] $TokenCode = (Read-Host "Token Code"),
 
 		[Parameter()]
 		[System.EnvironmentVariableTarget] $Environment = [System.EnvironmentVariableTarget]::Process,
@@ -19,19 +19,27 @@ function Set-AwsDefaultSession {
 
 	$token = Get-STSSessionToken -SerialNumber $SerialNumber -TokenCode $TokenCode
 
-	[Environment]::SetEnvironmentVariable("AWS_ACCESS_KEY_ID", $token.AccessKeyId, $Environment)
-	[Environment]::SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", $token.SecretAccessKey, $Environment)
-	[Environment]::SetEnvironmentVariable("AWS_SESSION_TOKEN", $token.SessionToken, $Environment)
-
 	if ($Session) {
+		Write-Verbose "Writing to remote host $($Session.ComputerName)"
 		Invoke-Command `
 			-Session $Session `
 			-ArgumentList $token, $Environment `
 			-ScriptBlock {
-				[Environment]::SetEnvironmentVariable("AWS_ACCESS_KEY_ID", $token.AccessKeyId, $Environment)
-				[Environment]::SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", $token.SecretAccessKey, $Environment)
-				[Environment]::SetEnvironmentVariable("AWS_SESSION_TOKEN", $token.SessionToken, $Environment)
+				param($Token, $Environment)
+				Write-Verbose "Setting AWS_ACCESS_KEY_ID to $($Token.AccessKeyId) for $Environment"
+				[Environment]::SetEnvironmentVariable("AWS_ACCESS_KEY_ID", $Token.AccessKeyId, $Environment)
+				Write-Verbose "Setting AWS_SECRET_ACCESS_KEY to $($Token.SecretAccessKey) for $Environment"
+				[Environment]::SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", $Token.SecretAccessKey, $Environment)
+				Write-Verbose "Setting AWS_SESSION_TOKEN to $($Token.SessionToken) for $Environment"
+				[Environment]::SetEnvironmentVariable("AWS_SESSION_TOKEN", $Token.SessionToken, $Environment)
 			}
+	} else {
+		Write-Verbose "Setting AWS_ACCESS_KEY_ID to $($Token.AccessKeyId) for $Environment"
+		[Environment]::SetEnvironmentVariable("AWS_ACCESS_KEY_ID", $Token.AccessKeyId, $Environment)
+		Write-Verbose "Setting AWS_SECRET_ACCESS_KEY to $($Token.SecretAccessKey) for $Environment"
+		[Environment]::SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", $Token.SecretAccessKey, $Environment)
+		Write-Verbose "Setting AWS_SESSION_TOKEN to $($Token.SessionToken) for $Environment"
+		[Environment]::SetEnvironmentVariable("AWS_SESSION_TOKEN", $Token.SessionToken, $Environment)
 	}
 
 	if ($PassThru) {
