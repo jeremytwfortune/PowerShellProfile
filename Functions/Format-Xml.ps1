fe007dd2fe361f8eb252d5dd4a1ec287ad691e93
Jeremy Fortune <jeremy@careevolution.com>
2020-04-03 09:06:31 -0400

    Fix Format-Xml
    Format-Xml previously was just... entirely broken. It isn't clear where
    this thing came from. Now this accepts pipeline input in either string
    or XML forms.

--- Functions/Format-Xml.ps1
+++ Functions/Format-Xml.ps1
@@ -1,11 +1,48 @@
-function Format-Xml ([xml]$xml, $indent=2)
-{
-    $StringWriter = New-Object System.IO.StringWriter
-    $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter
-    $xmlWriter.Formatting = “indented”
-    $xmlWriter.Indentation = $Indent
-    $xml.WriteContentTo($XmlWriter)
-    $XmlWriter.Flush()
-    $StringWriter.Flush()
-    Write-Output $StringWriter.ToString()
+function Format-Xml {
+	param(
+		[Parameter(
+			ValueFromPipeline,
+			ParameterSetName = "String")]
+		[string[]]$InputString,
+
+		[Parameter(
+			ValueFromPipeline,
+			ParameterSetName = "XML")]
+		[xml]$InputXml,
+
+		[Parameter()]
+		$Indent = 2,
+
+		[Parameter()]
+		$IndentChar = " "
+	)
+
+	begin {
+		$compiledString = ""
+	}
+
+	process {
+		$input = $_
+		switch ($PSCmdlet.ParameterSetName) {
+			"String" {
+				$compiledString += $input
+				$xml = [xml]$compiledString
+			}
+			"XML" {
+				$xml = $InputXml
+			}
+		}
+	}
+
+	end {
+		$stringWriter = New-Object System.IO.StringWriter
+		$xmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter
+		$xmlWriter.Formatting = "indented"
+		$xmlWriter.Indentation = $Indent
+		$xmlWriter.IndentChar = $IndentChar
+		$xml.WriteContentTo($XmlWriter)
+		$xmlWriter.Flush()
+		$stringWriter.Flush()
+		Write-Output $stringWriter.ToString()
+	}
 }
