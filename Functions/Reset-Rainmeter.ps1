0f3fafbf365db6d1806cdf04a73644409639c725
Jeremy Fortune <jeremy@careevolution.com>
2020-04-03 09:08:15 -0400

    Remove Rainmeter
    Replaced by simply hitting <WinKey> twice.

--- Functions/Reset-Rainmeter.ps1
+++
@@ -1,5 +0,0 @@
-function Reset-Rainmeter {
-	$baseInstallDirectory = "C:\Program Files\Rainmeter"
-	$rainmeter = ( Get-ChildItem -Recurse $baseInstallDirectory "Rainmeter.exe" ).FullName
-	& "$rainmeter" '!RefreshApp'
-}
