7c13e13a367ba9b6928dcebc02090478520b7f18
Jeremy Fortune <jeremy@careevolution.com>
2020-04-20 08:00:32 -0400

    Remove Pipeline from Rou

--- Functions/Remove-OtherUser.ps1
+++ Functions/Remove-OtherUser.ps1
@@ -1,24 +1,15 @@
 function Remove-OtherUser {
 	[CmdletBinding()]
-	param(
-		[Parameter(ValueFromPipeline)]
-		[string] $UserName
-	)
+	param()
 
-	begin {
-		$otherUsers = quser | ? {$_ -notmatch 'USERNAME' -and $_ -notmatch '^>'}
-	}
-
-	process {
-		$otherUsers | %{
+	quser |
+		? {$_ -notmatch 'USERNAME' -and $_ -notmatch '^>'} |
+		%{
 			$fields = $_.Trim() -split '\s+'
-			if ($UserName -and $UserName -eq $fields[0]) {
-				$sessionId = $fields[$fields.Count - 6]
-				Write-Verbose "Logging off user '$($fields[0])'"
-				& logoff $sessionId
-			}
+			$sessionId = $fields[$fields.Count - 6]
+			Write-Verbose "Logging off user '$($fields[0])'"
+			& logoff $sessionId
 		}
-	}
 }
 
 Set-Alias rou Remove-OtherUser
