5e9927d6b8142b5c9f3f1994b1e9e7e9c40e5c05
Jeremy Fortune <jeremy@careevolution.com>
2021-03-06 06:48:14 -0500

    Update Installed Packages from Last Install

--- Packages.ps1
+++ Packages.ps1
@@ -1,4 +1,7 @@
-param ( [Switch] $Work )
+param (
+	[ValidateSet("Home", "Work", "WorkSpace")]
+	$Work
+)
 
 Set-ExecutionPolicy Bypass; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
@@ -6,43 +9,51 @@ choco install -y `
 	googlechrome `
 	powershell-core `
 	microsoft-windows-terminal `
-	dashlane `
 	vscode `
 	7zip `
 	git `
-	slack `
 	ag `
 	gpg4win `
-	gimp `
-	licecap `
-	greenshot `
-	windirstat `
-	office365proplus `
 	vim `
-	autohotkey `
-	f.lux.install `
-	rainmeter
 
 refreshenv
 code --install-extension Shan.code-settings-sync
 
-Copy-Item "$PSScriptRoot\wt-admin.lnk" $HOME
-Copy-Item "$PSScriptRoot\AutoHotKey.ahk" "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+Install-Module -Name PSReadLine -Force
+Install-Module -Name PowerLine
 
-if ( $Work ) {
+Copy-Item "$PSScriptRoot\ProgramList.txt" $HOME
+Copy-Item "$PSScriptRoot\AwsAssumableRoles.txt" $HOME
+Copy-Item "$PSScriptRoot\global.gitconfig" "$HOME\.gitconfig"
+"{}" | Out-File "$Home\CeServers.json"
+
+if ( $Machine -in ("Work", "Home") ) {
 	choco install -y `
+		autohotkey `
+		office365proplus `
+		f.lux.install `
+		rainmeter `
+		windirstat `
+		greenshot `
+		slack `
+		gimp `
+		licecap `
+
+	Copy-Item "$PSScriptRoot\wt-admin.lnk" $HOME
+	Copy-Item "$PSScriptRoot\AutoHotKey.ahk" "$HOME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+}
+
+if ( $Machine -eq "Work" ) {
+	choco install -y `
+		amazon-workspaces `
 		visualstudio2019enterprise `
-		sql-server-management-studio `
-		rdcman `
-		r.studio `
 		citrix-receiver `
 		nuget.commandline `
-		dotnetcore-sdk
+		dotnetcore-sdk `
+		ssms
 
 	choco install -y nodejs --version 12.19.0
-	# CE rdcman rdg file available in CE Google Drive
-
-	Start-Process "https://www.microsoft.com/en-us/sql-server/sql-server-downloads"
 
 	"IIS-ASPNET45",
 	"Windows-Identity-Foundation",
@@ -55,6 +66,3 @@ if ( $Work ) {
 		Enable-WindowsOptionalFeature -Online -FeatureName $_ -All
 	}
 }
-
-Install-Module -Name PSReadLine -Force
-Install-Module -Name PowerLine
