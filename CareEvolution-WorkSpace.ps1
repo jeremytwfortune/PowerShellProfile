06235d42cf212661a6c82523a208294956fef2fa
Jeremy Fortune <jeremy@careevolution.com>
2021-03-07 09:23:25 -0500

    Remove Corp Credentials from CE WorkSpace

--- CareEvolution-WorkSpace.ps1
+++ CareEvolution-WorkSpace.ps1
@@ -1,12 +1,7 @@
-$Env:OCTOPUS_SERVERURL = "https://octopus.careevolution.com"
-$Env:OCTOPUS_APIKEY = $CredentialStore.Tokens.OctopusApiKey
-$Global:CredentialStore.Tokens.OctopusApiKey = Get-Secret "OctopusApiKey"
 $Global:CredentialStore.Tokens.Proget = Get-Secret "Proget"
 
 $global:StoredAWSRegion = 'us-east-1'
 
-$Global:CredentialStore.CeDownloader = New-Object PSCredential("CEDownloader", (Get-Secret "CEDownloader"))
-$Global:CredentialStore.CeCorp = New-Object PSCredential("jeremy@corp", (Get-Secret "Corp"))
 $Global:CredentialStore.CePep = New-Object PSCredential("jeremy.fortune@pep", (Get-Secret "Pep"))
 $Global:CredentialStore.Ce = New-Object PSCredential("jeremy", (Get-Secret "Corp"))
 
