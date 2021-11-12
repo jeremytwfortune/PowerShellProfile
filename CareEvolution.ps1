488375c6616ed584c3f96b774f3204884b4f0c87
Jeremy Fortune <jeremy@careevolution.com>
2021-11-11 21:29:08 -0500

    Add GE2C Deployments

--- CareEvolution.ps1
+++ CareEvolution.ps1
@@ -10,6 +10,18 @@ $Global:CredentialStore.CeCorp = New-Object PSCredential("jeremy@corp", (Get-Sec
 $Global:CredentialStore.CePep = New-Object PSCredential("jeremy.fortune@pep", (Get-Secret "Pep"))
 $Global:CredentialStore.Ce = New-Object PSCredential("jeremy", (Get-Secret "Corp"))
 
+$Global:Ge2cDeployment = @{
+	WebClientTestDev = "$Repos\SecurityAndChangeControl\src\aws\sandbox\ge2c\environments\dev\deployments\webclienttestdev"
+	MyFhrDev = "$Repos\SecurityAndChangeControl\src\aws\pep\qa\ge2c\environments\dev\deployments\myfhrdev"
+	MyFhrProd = "$Repos\SecurityAndChangeControl\src\aws\pep\galileo-prod\ge2c\environments\prod\deployments\myfhrprod"
+	TenantGroupTest = "$Repos\SecurityAndChangeControl\src\aws\galileo\ge2c\environments\integrationtest\deployments\tenantgrouptest"
+	VidantTest = "$Repos\SecurityAndChangeControl\src\aws\galileo\ge2c\environments\integrationtest\deployments\vidanttest"
+	CamdenHieTest = "$Repos\SecurityAndChangeControl\src\aws\galileo\ge2c\environments\integrationtest\deployments\CamdenHieTest"
+	TenantGroupProd = "$Repos\SecurityAndChangeControl\src\aws\galileo\ge2c\environments\prod\deployments\tenantgroupprod"
+	VidantProd = "$Repos\SecurityAndChangeControl\src\aws\galileo\ge2c\environments\prod\deployments\vidantprod"
+	CamdenHie = "$Repos\SecurityAndChangeControl\src\aws\galileo\ge2c\environments\prod\deployments\camdenhie"
+}
+
 if ( Test-Path $Home\CeServers.json ){
   $Global:CeServers = Get-Content $Home\CeServers.json | ConvertFrom-Json
 }
