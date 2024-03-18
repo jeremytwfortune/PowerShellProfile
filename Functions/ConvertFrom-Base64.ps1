965f4e3fdf4010d0fac8760a7989174493f137e1
Jeremy Fortune <jeremy@careevolution.com>
2024-03-18 10:33:50 -0400

    Add Support for Byte Base64

--- Functions/ConvertFrom-Base64.ps1
+++ Functions/ConvertFrom-Base64.ps1
@@ -2,10 +2,16 @@ function ConvertFrom-Base64 {
 	[CmdletBinding()]
 	param(
 		[Parameter(Mandatory, ValueFromPipeline)]
-		[string]$InputObject
+		[string]$InputObject,
+
+		[Parameter()]
+		[switch]$AsByteStream
 	)
 	process {
 		$bytes = [System.Convert]::FromBase64String($InputObject)
+		if ($AsByteStream) {
+			return $bytes
+		}
 		[System.Text.Encoding]::UTF8.GetString($bytes)
 	}
 }
--- Functions/ConvertTo-Base64.ps1
+++ Functions/ConvertTo-Base64.ps1
@@ -2,10 +2,18 @@ function ConvertTo-Base64 {
 	[CmdletBinding()]
 	param(
 		[Parameter(Mandatory, ValueFromPipeline)]
-		[string]$InputObject
+		[string]$InputObject,
+
+		[Parameter()]
+		[switch]$FromFileByteStream
 	)
 	process {
-		$bytes = [System.Text.Encoding]::UTF8.GetBytes($InputObject)
+		if ($FromFileByteStream) {
+			$bytes = Get-Content -Path $InputObject -AsByteStream -ReadCount 0
+		}
+		else {
+			$bytes = [System.Text.Encoding]::UTF8.GetBytes($InputObject)
+		}
 		[System.Convert]::ToBase64String($bytes)
 	}
 }
